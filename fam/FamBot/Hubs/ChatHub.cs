da8ccd18b663a65367613b43fde01f4eb99daa9b
Dfreem <freemand@my.lanecc.edu>
2023-06-25 09:33:17 -0700

    --wip-- [skip ci]

--- fam/FamBot/Hubs/ChatHub.cs
+++ fam/FamBot/Hubs/ChatHub.cs
@@ -4,8 +4,8 @@ namespace FamBot.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.All.SendAsync("ReceiveMessage", message);
     }
 }
